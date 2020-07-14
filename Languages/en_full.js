bd0b0542931f14e357104f87bf069ad230fc6ae0
Jisco <JensHoernecke@gmx.de>
2020-07-14 16:15:25 +0200

    add english translation with components

---
+++ Languages/en_full.js
@@ -0,0 +1,479 @@
+﻿export const Dictionary = {
+ devextreme: {
+        search: "Search...",
+        grouping: {
+            groupByThisColumn: "Group by This Column",
+            groupContinuedMessage: "Continued from the previous page",
+            groupContinuesMessage: "Continues on the next page",
+            ungroup: "Ungroup",
+            ungroupAll: "Ungroup All",
+            emptyPanelText: "Drag a column header here to group by that column"
+        },
+        columnchooser: {
+            emptyPanelText: "Drag a column here to hide it",
+            title: "Column Chooser"
+        },
+        columnfixing: {
+            fix: "Fix",
+            leftPosition: "To the left",
+            rightPosition: "To the right",
+            unfix: "Unfix"
+        },
+        gridEditing: {
+            addRow: "Add a row",
+            cancelAllChanges: "Discard changes",
+            cancelRowChanges: "Cancel",
+            confirmDeleteMessage: "Are you sure you want to delete this record?",
+            confirmDeleteTitle: "",
+            deleteRow: "Delete",
+            editRow: "Edit",
+            saveAllChanges: "Save changes",
+            saveRowChanges: "Save",
+            undeleteRow: "Undelete",
+            validationCancelChanges: "Cancel changes"
+        },
+        filterRow: {
+            applyFilterText: "Apply filter",
+            betweenEndText: "End",
+            betweenStartText: "Start",
+            operationDescriptions: {
+                between: "Between",
+                contains: "Contains",
+                endsWith: "Ends with",
+                equal: "Equals",
+                greaterThan: "Greater than",
+                greaterThanOrEqual: "Greater than or equal to",
+                lessThan: "Less than",
+                lessThanOrEqual: "Less than or equal to",
+                notContains: "Does not contain",
+                notEqual: "Does not equal",
+                startsWith: "Starts with",
+            },
+            resetOperationText: "Reset",
+        },
+        summary: {
+            avg: "Avg={0}",
+            avgOtherColumn: "Avg of {1} is {0}",
+            count: "Count={0}",
+            max: "Max={0}",
+            maxOtherColumn: "Max of {1} is {0}",
+            min: "Min={0}",
+            minOtherColumn: "Min of {1} is {0}",
+            sum: "Sum={0}",
+            sumOtherColumn: "Sum of {1} is {0}"
+        },
+        noData: "No data",
+        select: "Select",
+        sorting: {
+            ascendingText: "Sort Ascending",
+            clearText: "Clear Sorting",
+            descendingText: "Sort Descending",
+        },
+        fileuploader: {
+            invalidFileExtensionMessage: "File type is not allowed",
+            invalidMaxFileSizeMessage: "File is too large",
+            invalidMinFileSizeMessage: "File is too small",
+            labelText: "or Drop file here",
+            readyToUploadMessage: "Ready to upload",
+            selectButtonText: "Select File",
+            uploadButtonText: "Upload",
+            uploadedMessage: "Uploaded",
+            uploadFailedMessage: "Upload failed",
+        }
+    },
+    newVersionTitle: "New Version available",
+    newVersion: "A new version is available.",
+    innovations: "Innovations",
+    installWhenClosing: "Install after closing",
+    installNowAndRestart: "Install now",
+    new: 'New',
+    load: 'Load',
+    cancel: 'Cancel',
+    save: 'Save',
+    saveAndClose: 'Save and close',
+    options: 'Options',
+    ok: 'Ok',
+    create: 'Create',
+    yes: 'Yes',
+    no: 'No',
+    unknown: 'Unknown',
+    chosenOne: 'Main person',
+    importFile: 'Import file',
+    exportFile: 'Export file',
+    value: 'Value',
+    count: 'Actual usings',
+    empty: 'Nothing selected',
+    optionModals: {
+        flags: {
+            title: 'Person Flags',
+            name: 'Name',
+            color: 'Color'
+        }
+    },
+    routing: {
+        Chart: 'Loading Chartview...',
+        PersonDatagrid: 'Loading Tableview...',
+        Export: 'Loading Export...',
+        Preview: 'Loading Preview...',
+        Families: 'Loading Families...',
+        Documents: 'Loading Documents...',
+        Suggestions: 'Loading Suggestions...',
+    },
+    suggestions: {
+        cities: 'Cities',
+        countries: 'Countries',
+        citiesInCountry: 'City in Country',
+        firstNames: 'Firstnames',
+        lastNames: 'Lastnames',
+        religions: 'Religions',
+        education: 'Educations',
+        jobs: 'Jobs',
+        streets: 'Street, Numbers and Cities',
+        postcodes: 'Postcodes and Cities',
+        alreadyExists: {
+            title: 'Hint',
+            text: 'This dataset already exists.'
+        },
+        updatePersons: 'Update value also in persons',
+        categories: {
+            names: 'Names',
+            expressions: 'Titles',
+            geographie: 'Geography'
+        },
+        deleteAllWithoutUsing: 'Delete all values without using'
+    },
+    import: {
+        dialog: 'Welcome',
+        new: 'Create new dataset',
+        load: 'Import dataset',
+        loadingIndicator: 'Copy data',
+        savingIndicator: 'Save data'
+    },
+    confirm: {
+        person: {
+            delete: {
+                title: 'Really remove the person?',
+                text: 'Should the person really be removed?'
+            }
+        }
+    },
+    editPerson: {
+        options: {
+            delete: 'Delete Person'
+        },
+        tabs: {
+            common: {
+                title: 'Common'
+            },
+            parents: {
+                title: 'Parents',
+                biological: 'Biological Parents'
+            },
+            images: {
+                title: 'Images',
+                loadimages: 'Loading Images...',
+                show: 'Show',
+                delete: 'Delete',
+                favorite: 'Favorite'
+            },
+            birth: {
+                title: 'Birth'
+            },
+            religion: {
+                title: 'Religion'
+            },
+            death: {
+                title: 'Death'
+            },
+            funeral: {
+                title: 'Funeral'
+            },
+            weddings: {
+                title: 'Weddings'
+            },
+            divorces: {
+                title: 'Divorces'
+            },
+            partners: {
+                title: 'Partners'
+            },
+            jobs: {
+                title: 'Jobs'
+            },
+            education: {
+                title: 'Education'
+            },
+            addresses: {
+                title: 'Addresses'
+            },
+            notes: {
+                title: 'Notes'
+            }
+        }
+    },
+    menu: {
+        files: {
+            title: 'File',
+            close: 'Close'
+        },
+        database: {
+            title: 'Databases',
+            new: 'New',
+            families: 'Families',
+            import: 'Import',
+            export: 'Export'
+        },
+        family: {
+            title: 'Family',
+            chart: 'Chart',
+            table: 'Table',
+            suggestions: 'Suggestions',
+            documents: 'Documents',
+            statistics: 'Statistics',
+            validations: 'Validations'
+        },
+        languages: 'Language',
+        export: 'Export',
+        help: {
+            title: 'Help',
+            thirdPartySoftware: 'Third Party Software',
+            about: 'About "Visual Family Tree"',
+        }
+    },
+    chart: {
+        load: "Loading Chart...",
+        settings: {
+            refresh: "Refresh"
+        },
+        leftBar: {
+            actions: 'Add',
+            settings: 'Settings' 
+        },
+        rightBar: {
+            title: 'Edit'
+        },
+        dragDrop: {
+            addChildren: 'Child',
+            addParent: 'Parent',
+            addPartner: 'Partner',
+            alreadyTwoParents: 'The person already has two biological parents.',
+            notPossibleActionTitle: 'This action could not be carried out!',
+            removePerson: 'Delete'
+        },
+        options: {
+            familyTree: "Family Tree",
+            ancestorsTree: "Ancestors",
+            title: "Chart-Options",
+            compactView: "Compact-View",
+            showUnknownPersons: "Draw unknown persons",
+            ifUnknownDrawNode: "Unkown persons are visible",
+            ifUnknownDrawInvisibleNode: "Unkown persons are invisible",
+            showPartner: "Show Partner",
+            fastAdd: "Fast Add",
+            fastDelete: "Fast Delete",
+            openFastEdit: "Open fast editing automatically"
+        }
+    },
+    footer: {
+        actualDataset: 'Dataset'
+    },
+    newPerson: {
+        title: 'Create new person',
+        next: 'Create another person'
+    },
+    newChild: {
+        title: 'New descendants for ',
+        next: 'Create another descendant',
+        otherParent: 'Other Parent'
+    },
+    newPartner: {
+        title: 'New partner for ',
+        next: 'Create another partner',
+        existing: 'Existing Person'
+    },
+    newParent: {
+        title: 'New parent for '
+    },
+    newSibling: {
+        title: 'New sibling for ',
+        next: 'Create another sibling'
+    },
+    newFamily: {        
+        title: 'Create family',
+        name: 'Family name'
+    },
+    firstPerson: {
+        title: 'Create first person'
+    },
+    person: {
+        AddSourceLink: 'Add source',
+        Link: 'Source',
+        Weddingday: 'Weddingday',
+        Flags: 'Flags',
+        FirstNames: 'FirstName(s)',
+        LastName: 'Lastname',
+        Birthname: 'Maiden name',
+        Time: 'Time',
+        Name: 'Full Name',
+        Notes: 'Notes',
+        Country: 'Country',
+        Place: 'Place',
+        ReligionName: 'Religion',
+        Events: 'Events',
+        Date: 'Date',
+        From: 'From',
+        To: 'To',
+        Title: 'Title',
+        Partner: 'Partner',
+        Birthdate: 'Birthdate',
+        Deathdate: 'Deathdate',
+        Weddingdate: 'Weddingdate',
+        Parent1: 'First Parent',
+        Parent2: 'Second Parent',
+        AddParent: 'Add Parent',
+        AddPartner: 'Add Partner',
+        OnlyYear: 'only year',
+        OnlyBirthYear: 'Only the year of birth is known',
+        OnlyDeathYear: 'Only the year of death is known',
+        OnlyFuneralYear: 'Only the year of funeral is known',
+        Funeraldate: 'Funeraldate',
+        BirthPlace: 'Birth Place',
+        DeathPlace: 'Death Place',
+        FuneralPlace: 'Funeral Place',
+        BirthCountry: 'Birth Country',
+        DeathCountry: 'Death Country',
+        FuneralCountry: 'Funeral Country',
+        Street: 'Street and Number',
+        PostCode: 'Post Code',
+        NoPartner: {
+            title: 'No Partner',
+            text: 'A partner must be created beforehand.'
+        }
+    },
+    abbreviations: {
+        Birthname: 'née'
+    },
+    requires: {
+        FirstNames: 'Please insert an firstname',
+        LastName: 'Please insert the lastname',
+        familyName: 'Please insert the family name'
+    },
+    print: {
+        EndEditing: 'End editing',
+        StartEditing: 'Start editing',
+        loadImage: 'Load Image...',
+        creatingBackgroundImage: 'Create Backgroundimage...',
+        edit: {
+            delete: 'Delete',
+            undo: 'Undo',
+            redo: 'Redo',
+            reset: 'Reset',
+            hidePaths: 'Show Lines',
+            hideTexts: 'Show Texts',
+            hideImages: 'Show Images'
+        },
+        Editable: "Editable",
+        CreatingPreview: "Creating Vector Graphic...",
+        settings: "Settings",
+        expertSettings: "Advanced Settings",
+        type: "Type",
+        types: {
+            ancestor: "Ancestor Tree",
+            familyTree: "Family Tree"
+        },
+        rootPerson: "Root Person",
+        direction: "Orientation",
+        directions: {
+            left: "Left to Right",
+            top: "Top to Bottom",
+            bottom: "Bottom to Top",
+            right: "Right To Left"
+        },
+        designSettings: "Design",
+        nodeDesign: "Design",
+        create: "Create",
+        designs: {
+            small: "Slim",
+            wide: "Wide",
+            custom: "Custom"
+        },
+        backgrounds: {
+            standard: "Standard",
+            image: "Image",
+            color: "Color",
+            tree: "Tree",
+            none: "None",
+            own: "Own Image"
+        },
+        Preview: "Preview",
+        BackToSettings: "Back to settings",
+        UserDefinedSettings: {
+            Stacked: "Stacked",
+            StackedTop: "Start Top",
+            StackedLeft: "Start Left",
+            ShowAlternativeDatafield: "Show '%0%' if filled",
+            Scaling: "Scaling",
+            Direct: "Directly",
+            Indirectly: "Indirectly",
+            ConnectingLines: "Connection Lines",
+            Rounding: "Rounding",
+            Background: "Background",
+            Top: "Top",
+            Left: "Left",
+            Image: "Image",
+            Border: "Border",
+            Visible: "Visible",
+            Height: "Height",
+            Width: "Width",
+            Color: "Color",
+            Thickness: "Thickness",
+            Texts: "Texts",
+            Distance: "Distance",
+            Separator: "Separator",
+            Font: "Font",
+            FontFamily: "Font-Family",
+            FontSize: "Fontsize",
+            Italic: "Italic",
+            Bold: "Bold",
+            DateFormat: "Dateformat",
+            ResetToSmall: "Reset to Slim",
+            ResetToWide: "Reset to Wide",
+            HorizontalAlignment: "Horizontal Alignment",
+            VerticalAlignment: "Vertical Alignment",
+            Opacity: "Opacity",
+            HorizontalAlignments: {
+                None: "None",
+                Left: "Left",
+                Right: "Right",
+                Center: "Center",
+                Stretch: "Stretch"
+            },
+            VerticalAlignments: {
+                None: "None",
+                Top: "Top",
+                Bottom: "Bottom",
+                Center: "Center",
+                Stretch: "Stretch"
+            }
+        },
+        Download: {
+            AsPng: "Save as Image",
+            AsSvg: "Save as Vector",
+            AsPdf: "Save as PDF"
+        },
+        Generate: {
+            Png: "Creating Image...",
+            Svg: "Creating Vector...",
+            Pdf: "Creating PDF..."
+        }
+    },
+    thirdParty: {
+        grafics: "Some Icons and Images",
+        fonts: "Fonts",
+        font: "Font",
+        licence: "Licence"
+    },
+    about: {
+        buildBy: "'Visual Family Tree' is built by Jens Hoernecke"
+    }
+};
